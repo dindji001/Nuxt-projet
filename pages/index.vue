313c0992aa26e309e793811adb312b24d9cc105a
severin <severindindji68@gmail.com>
2022-03-14 00:38:21 +0000

    init

---
+++ pages/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <main>
+    <h1>TO-DO-LIST APP</h1>
+    <p>Creer une liste de tache</p>
+
+    <div class="create-new">
+      <input
+        type="text"
+        v-model="newTask"
+        placeholder="Ajouter une tache"
+        @keypress.enter="addTask" />
+      <button @click="addTask">Ajouter</button>
+    </div>
+
+    <div class="tasks">
+      <Task
+        v-for="(task, i) in $store.state.tasks"
+        :key="i"
+        :task="task" />
+    </div>
+
+  </main>
+</template>
+
+<script>
+export default {
+  data (){
+    return {
+      newTask: ''
+    }
+  },
+  methods: {
+    addTask () {
+      if (this.newTask) {
+        this.$store.commit('ADD_TASK', this.newTask);
+        this.newTask = '';
+      }
+    }
+  }
+}
+</script>
